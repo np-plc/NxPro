31ae2e361480b55dd90bcee0564304cf1efa67d0
C Field <106707280+cfield1515@users.noreply.github.com>
2025-11-22 00:51:35 +0000

    Added Folders + Providers

---
+++ Providers/Providers.lua
@@ -0,0 +1,25 @@
+local module = {
+	
+	["1"] = {
+		["NAme"] = "NETPOX",
+		["Logo"] = "rbxassetid://118103660548946"
+	},
+	
+	["2"] = {
+		["NAme"] = "TESCO",
+		["Logo"] = "rbxassetid://126713141398208"
+	},
+	
+	["3"] = {
+		["NAme"] = "SAINSBURYS",
+		["Logo"] = "rbxassetid://104520071736767"
+	},
+	
+	["4"] = {
+		["NAme"] = "MAIE",
+		["Logo"] = "rbxassetid://90765623114324"
+	},
+	
+}
+
+return module
