098641759697356e0ead81f7bdc398c7036c5fc0
NetPox PLC <cfield791@gmail.com>
2025-11-22 02:56:21 +0000

    Update Providers.lua

--- Providers/Providers.lua
+++ Providers/Providers.lua
@@ -1,23 +1,38 @@
 local module = {
 	
 	["1"] = {
-		["NAme"] = "NETPOX",
+		["NAme"] = "NETPOX", -- NetPox
 		["Logo"] = "rbxassetid://118103660548946"
 	},
 	
 	["2"] = {
-		["NAme"] = "TESCO",
+		["NAme"] = "TESCO", -- Tesco
 		["Logo"] = "rbxassetid://126713141398208"
 	},
 	
 	["3"] = {
-		["NAme"] = "SAINSBURYS",
+		["NAme"] = "SAINSBURYS", -- Sainsburys
 		["Logo"] = "rbxassetid://104520071736767"
 	},
 	
 	["4"] = {
-		["NAme"] = "MAIE",
+		["NAme"] = "MAIE", -- Maie
 		["Logo"] = "rbxassetid://90765623114324"
+	},
+
+	["5487"] = {
+		["NAme"] = "QUANTUM", -- Quantum
+		["Logo"] = "rbxassetid://129583102212903"
+	},
+
+	["5449"] = {
+		["NAme"] = "QUANTUM", -- Wilson1
+		["Logo"] = "rbxassetid://134410789987815"
+	},
+
+	["5448"] = {
+		["NAme"] = "QUANTUM", -- Wilson2
+		["Logo"] = "rbxassetid://98502374532316"
 	},
 	
 }
